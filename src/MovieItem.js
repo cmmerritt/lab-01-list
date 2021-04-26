0d6e4bc72c4a3d74da6c2914c1de0bf32eb5a36d
cmmerritt <christiane.merritt@gmail.com>
2021-04-26 15:19:14 -0700

    creature item component design

--- src/MovieItem.js
+++ src/MovieItem.js
@@ -5,11 +5,18 @@ class MovieItem extends Component {
 
   render() {
     return (
-      <li className="MovieItem ">
-        <h2>Psycho</h2>
-        <img src="https://cdn.shopify.com/s/files/1/1416/8662/products/psycho_1960_linen_original_film_art_1200x.jpg?v=1593534654" alt="Psycho, Alfred Hitchcock, 1960"/>
-        <p>Year: 1960</p>
-      </li>
+      <p>
+        <li className="MovieItem">
+          <h2>Psycho</h2>
+          <img src="https://cdn.shopify.com/s/files/1/1416/8662/products/psycho_1960_linen_original_film_art_1200x.jpg?v=1593534654" alt="Psycho, Alfred Hitchcock, 1960"/>
+          <p>Year: 1960</p>
+        </li>
+        <li className="MovieItem">
+          <h2>Psycho</h2>
+          <img src="https://cdn.shopify.com/s/files/1/1416/8662/products/psycho_1960_linen_original_film_art_1200x.jpg?v=1593534654" alt="Psycho, Alfred Hitchcock, 1960"/>
+          <p>Year: 1960</p>
+        </li>
+      </p>
     );
   }
 
