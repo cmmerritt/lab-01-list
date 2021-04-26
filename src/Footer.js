a6fa7dba5bd038c39e9df3e48db5cc1d49311bbb
cmmerritt <christiane.merritt@gmail.com>
2021-04-26 14:03:42 -0700

    basic app structure

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+
+  render() {
+    return (
+      <footer className="footer">
+        &copy; 2021 Horror Movies
+      </footer>
+    );
+  };
+
+}
+
+export default Footer;
